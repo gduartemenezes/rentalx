f3e76cff9d558aa28a6444c5f6a09368a00f3a28
Gustavo Duarte <gduartemenezes@gmail.com>
2021-09-12 17:28:00 -0300

    refactor: add Category class to set a type pattern for created categories

--- src/routes/cetegories.routes.ts
+++ src/routes/cetegories.routes.ts
@@ -1,21 +1,23 @@
-import { response, Router } from 'express'
+import { Router } from 'express'
 import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../model/Category'
 
 const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body
-    const category = {
+    const category: Category = {
         name,
         description,
-        id: uuidV4()
+        id: uuidV4(),
+        created_at: new Date()
     }
 
     categories.push(category)
 
-    return response.status(201).send({category})
+    return response.status(201).send(category)
 })
 
 export { categoriesRoutes }
