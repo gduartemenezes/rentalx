b7256aaed00f002d2e4be0069dd3b25ef15ea969
Gustavo Duarte <gduartemenezes@gmail.com>
2021-09-12 18:27:27 -0300

    refactor: use CategoriesRepository to handle category's creation

--- src/routes/cetegories.routes.ts
+++ src/routes/cetegories.routes.ts
@@ -1,22 +1,13 @@
-import { Router } from 'express'
-import { Category } from '../model/Category'
+import { Router } from "express";
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 
-const categoriesRoutes = Router()
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository()
 
-const categories: Category[] = []
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+  categoriesRepository.create({name, description})
+  return response.status(201).send();
+});
 
-categoriesRoutes.post('/', (request, response) => {
-    const { name, description } = request.body
-    const category = new Category()
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date()
-    })
-
-    categories.push(category)
-
-    return response.status(201).send(category)
-})
-
-export { categoriesRoutes }
+export { categoriesRoutes };
