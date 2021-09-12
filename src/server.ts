199f4268ce1159fa9cf72b168352b123f41ff8cd
Gustavo Duarte <gduartemenezes@gmail.com>
2021-09-12 16:55:21 -0300

    feat: consume categories.routes in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/cetegories.routes"
 
 const app = express()
 
+app.use(express.json())
 
-app.get("/", (request, response) => {
-    return response.json({message: "Hello World"})
-})
+app.use(categoriesRoutes)
 
 app.listen(3333, () => console.log("Server is running!"))
