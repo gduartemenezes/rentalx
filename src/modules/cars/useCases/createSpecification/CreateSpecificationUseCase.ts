e21c7e10eff2c9eafab9b06c7fa71062a2b8d387
Gustavo Duarte <gduartemenezes@gmail.com>
2021-09-15 21:25:18 -0300

    feat: add existence validation

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,12 +1,21 @@
 import { ISpecificationsRepository } from "../../repositories/ISpecificantionsRepository";
 
-
-class CreateSpecificationUseCase {
-    constructor (private specificationsRepository: ISpecificationsRepository){}
-    
-    execute({name, description}: IRequest): void {
-        this.specificationsRepository.create({name, description})
-    }
+interface IRequest {
+  name: string;
+  description: string;
 }
 
-export { CreateSpecificationUseCase }
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists')
+    }
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
