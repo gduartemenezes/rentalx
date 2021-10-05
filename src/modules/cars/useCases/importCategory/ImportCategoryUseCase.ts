cedb9f783b51c88e5a70ea0672215b6a53194b03
Gustavo Duarte <gduartemenezes@gmail.com>
2021-10-05 20:45:00 -0300

    refactor: use new Promise to ensure the stream's end, turn execute into an async function

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -9,22 +9,32 @@ interface IImportCategory {
 
 class ImportCategoryUseCase {
   constructor(private categoryRepository: ICategoriesRepository) {}
-  loadCategories(file: Express.Multer.File) {
-    const stream = fs.createReadStream(file.path);
-    const categories: IImportCategory[] = []
-    const parseFile = csvParse();
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const categories: IImportCategory[] = [];
+      const parseFile = csvParse();
 
-    stream.pipe(parseFile);
-    parseFile.on("data", async (line) => {
-      const [name, description] = line
-      categories.push({
-        name,
-        description
+      stream.pipe(parseFile);
+      parseFile.on("data", async (line) => {
+        const [name, description] = line;
+        categories.push({
+          name,
+          description,
+        })
       })
-    });
+      .on("end", () => {
+        resolve(categories)
+    }).on("error", (err) => {
+      reject(err)
+    })
+    })
   }
 
-  execute(file: Express.Multer.File): void {}
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
+  }
 }
 
 export { ImportCategoryUseCase };
