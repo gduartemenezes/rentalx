64934ce0abd007df43bea3e5ff22872eab808525
Gustavo Duarte <gduartemenezes@gmail.com>
2021-10-04 22:00:12 -0300

    refactor: use ImportCategory controller to handle multipart requests

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,8 +1,18 @@
+import { Request, Response } from "express";
 
 
 
 class ImportCategoryController {
-  constructor(private importCategoryUseCase: ImportCategoryUseCase) {
-    this.importCategoryUseCase
+  // constructor(private importCategoryUseCase: ImportCategoryUseCase) {
+  //   this.importCategoryUseCase
+  // }
+
+  handle(request: Request, response: Response): Response {
+    const {file} = request
+    console.log(file) 
+    return response.send()
   }
 }
+
+
+export { ImportCategoryController }
