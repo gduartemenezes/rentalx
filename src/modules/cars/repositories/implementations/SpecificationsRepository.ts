86596e3f7b68335e2280ac1bc61a8c70bb6db317
Gustavo Duarte <gduartemenezes@gmail.com>
2021-10-10 16:56:50 -0300

    Revert "refactor: implement async behavior to SpecificationsRepository"
    This reverts commit fce7164e47ae236b1787101ec75b23c592fdc8ba.

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,22 +1,24 @@
-import { getRepository, Repository } from "typeorm";
-import { Specification } from "../../entities/Specification";
+import { Specification } from "../../model/Specification";
 import { ICreateSpecificationDTO, ISpecificationsRepository } from "../ISpecificantionsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private repository: Repository<Specification>
+    private specifications: Specification[]
 
     constructor() {
-        this.repository = getRepository(Specification)
+        this.specifications = []
     }
     
-    async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-        const specification = this.repository.create({
-            name, description
+    create({ name, description }: ICreateSpecificationDTO): void {
+        const specification = new Specification()
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date()
         })
-        await this.repository.save(specification)
+        this.specifications.push(specification)
     }
-    async findByName(name: string): Promise<Specification> {
-        const specification = this.repository.findOne({ name })
+    findByName(name: string): Specification {
+        const specification = this.specifications.find(el => el.name === name)
         return specification
     }
     
