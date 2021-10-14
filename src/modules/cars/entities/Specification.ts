ff092376093a767b952d64c4d70c5e5d3500cc6e
Gustavo Duarte <gduartemenezes@gmail.com>
2021-10-13 21:04:15 -0300

    refactor: add typeorm references to Specification Entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,22 @@
 import { v4 as uuidV4 } from 'uuid';
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm"
 
+@Entity("specifications")
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+  
+  @Column()
   description: string;
+  
+  @CreateDateColumn()
   created_at: Date;
+  
+  @UpdateDateColumn()
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
